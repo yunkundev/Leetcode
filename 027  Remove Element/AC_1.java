32a6552f7a5dbe9dfabb1cc6eb676d503e5cb972
郭云坤 <yunkunguo@guest-wireless-upc-1604-10-120-077-179.usc.edu>
2017-04-01 22:38:11 -0700

    Solution 027 3/20

---
+++ 027  Remove Element/AC_1.java
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int removeElement(int[] nums, int val)
+    {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i]  == val )
+            {
+                continue;
+            }
+            else
+            {
+                nums[count++] = nums[i];
+            }
+
+        }
+        return count;
+    }
+}
